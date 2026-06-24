85c453c5c2c8762c1e347fec7ae559056550922f
Mirza Zain <zainmustafa205@gmail.com>
2026-06-24 12:27:47 +0500

    feat: add request and response DTOs for auth endpoints

---
+++ src/main/java/com/ecommerce/user_service/dto/response/AuthResponse.java
@@ -0,0 +1,18 @@
+package com.ecommerce.user_service.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthResponse {
+
+    private String token;
+    private String tokenType = "Bearer";
+    private UserResponse user;
+    
+}
