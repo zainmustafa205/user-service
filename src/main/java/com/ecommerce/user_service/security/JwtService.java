c2a3fb84f265eaabdfd864fa7e81d1189b6f71e1
Mirza Zain <zainmustafa205@gmail.com>
2026-07-13 13:11:18 +0500

    refactor: remove unused single-arg generateToken method from JwtService

--- src/main/java/com/ecommerce/user_service/security/JwtService.java
+++ src/main/java/com/ecommerce/user_service/security/JwtService.java
@@ -33,10 +33,6 @@ public class JwtService {
         return claimsResolver.apply(claims);
     }
 
-    public String generateToken(UserDetails userDetails) {
-        return generateToken(new HashMap<>(), userDetails);
-    }
-
     public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
         return Jwts.builder()
                 .claims(extraClaims)
